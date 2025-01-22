c04aa709b68cbe87767ab85a587331e566a69b57
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2025-01-22 12:26:45 +0200

    adding the lcoations

--- views/components/location.js
+++ views/components/location.js
@@ -3,16 +3,30 @@
  *
  * The rendered HTML includes:
  * - A list item (`<li>`) with the class `location-item`.
- * - A button that triggers an HTMX `hx-post` request to the `/places` endpoint with the location ID as a parameter.
+ * - A button that triggers an HTMX request based on the availability of the location.
  * - An image element displaying the location's image.
  * - A heading element displaying the location's title.
  *
- * HTMX attributes used:
- * - `hx-post`: Specifies the URL to which the POST request is sent.
- * - `hx-vals`: Specifies the values to be sent with the request, in this case, the location ID.
- * - `hx-target`: Specifies the target element where the response will be placed.
- * - `hx-swap`: Specifies how the response will be inserted into the target element.
-  */
+ * If the location is available (`isAvailableLocation` is `true`):
+ * - The button triggers an HTMX `hx-post` request to the `/places` endpoint with the location ID as a parameter.
+ * - HTMX attributes used:
+ *   - `hx-post`: Specifies the URL to which the POST request is sent.
+ *   - `hx-vals`: Specifies the values to be sent with the request, in this case, the location ID.
+ *   - `hx-target`: Specifies the target element where the response will be placed.
+ *   - `hx-swap`: Specifies how the response will be inserted into the target element.
+ *
+ * If the location is not available (`isAvailableLocation` is `false`):
+ * - The button triggers an HTMX `hx-delete` request to the `/places/{location.id}` endpoint.
+ * - HTMX attributes used:
+ *   - `hx-delete`: Specifies the URL to which the DELETE request is sent.
+ *   - `hx-confirm`: Displays a confirmation dialog before sending the request.
+ *   - `hx-target`: Specifies the closest `li` element as the target element.
+ *   - `hx-swap`: Specifies how the response will be inserted into the target element.
+ *
+ * @param {Object} location - The location object containing details to be rendered.
+ * @param {boolean} [isAvailableLocation=true] - Flag indicating if the location is available.
+ * @returns {string} The HTML string representing the location item.
+ */
 export default function renderLocation(location, isAvailableLocation = true) {
   let attributes;
 
@@ -41,4 +55,3 @@ export default function renderLocation(location, isAvailableLocation = true) {
     </li>
   `;
 }
-
